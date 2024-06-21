02615ba412cdba90560ed8499f051448f71832ed
Amiir25 <amirsadik636@gmail.com>
2024-06-21 03:53:54 +0300

    Add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/**
+ * File: 5-printf.c
+ * Author: Amir Sadik
+ * This file contains a C program that displays a message
+ */
+
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
