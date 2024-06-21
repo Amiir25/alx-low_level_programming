e950ce01934f6c4366e97ba664f380afa2521589
Amiir25 <amirsadik636@gmail.com>
2024-06-21 17:54:06 +0300

    Update on 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,13 @@
-/**
- * File: 5-printf.c
- * Author: Amir Sadik
- * This file contains a C program that displays a message
- */
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
