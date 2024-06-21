6839a88c9a81100ff58f310f2eb2404fc172f713
Amiir25 <amirsadik636@gmail.com>
2024-06-21 03:39:19 +0300

    Update on 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,17 @@
+/**
+ * File: 4-puts.c
+ * Author: Amir Sadik
+ * This file contains a C program that displays a message
+ */
+
 #include <stdio.h>
 
 /**
- * This program displays a message
+ * main - Entry point
+ *
+ * This function prints a simple message
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
