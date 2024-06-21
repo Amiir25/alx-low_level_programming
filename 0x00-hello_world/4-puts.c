0266e61dd2205c3d566f0c20963ddd5793fe67e3
Amiir25 <amirsadik636@gmail.com>
2024-06-21 17:50:10 +0300

    Update on 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,15 @@
-/**
- * File: 4-puts.c
- * Author: Amir Sadik
- * This file contains a C program that displays a message
- */
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
  *
- * This function prints a simple message
- *
- * Return:
- * Always 0 (Success)
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like bulding a multilingual puzzle");
 	return (0);
 }
