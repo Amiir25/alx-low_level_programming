197c3c671a6d3b09bcd5ef93baeec3696e0a07b1
Amiir25 <amirsadik636@gmail.com>
2024-06-25 14:36:41 +0300

    Update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,16 +6,16 @@
  * Return: Always 0 (Success)
  */
 
-int main()
+int main(void)
 {
 	char str[] = "_putchar";
 	int i;
 
-	for (i = 0; str[i] != '\0'; i ++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
 	_putchar('\n');
-	
-	return 0;
+
+	return (0);
 }
