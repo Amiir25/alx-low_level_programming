ac02d8374f167805eeda3c039a1e8e4681d68852
Amiir25 <amirsadik636@gmail.com>
2024-06-25 18:13:35 +0300

    Add 7-print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Checks if a number is positive negative or zero.
+ * @n: The character to be checked
+ *
+ * Return: int
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
