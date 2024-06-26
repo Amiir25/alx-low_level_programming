160486404652144fb02c6936b72672b32bb5b9a8
Amiir25 <amirsadik636@gmail.com>
2024-06-26 17:46:05 +0300

    Add 3-print remaining days.c

---
+++ 0x03-debugging/3-print_remaining_days.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_remaining_days - prints the remaining days of the year
+ * @year: a variable to store years
+ * @month: a variable to store months
+ * @day: a variable to store days
+ */
+
+void print_remaining_days(int month, int day, int year)
+{
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+	{
+		if (month > 2 || (month == 2 && day == 29))
+		{
+			day++;
+		}
+
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
+	}
+	else
+	{
+		if (month == 2 && day == 29)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
+		}
+	}
+}
