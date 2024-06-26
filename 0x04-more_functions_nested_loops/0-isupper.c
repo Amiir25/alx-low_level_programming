1556bf1e6ac9ead0c93b7740415a5c611a48cb68
Amiir25 <amirsadik636@gmail.com>
2024-06-26 20:55:23 +0300

    Add 0-_isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a letter is uppercase
+ * @c: character variable
+ *
+ * Returns: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c == 'A')
+		return (1);
+	else
+		return (0);
+}
