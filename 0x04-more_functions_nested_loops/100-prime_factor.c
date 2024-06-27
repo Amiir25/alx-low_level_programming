488d10cb602eb3e077b13c589a1bc5114c0212cd
Amiir25 <amirsadik636@gmail.com>
2024-06-27 18:49:16 +0300

    Update

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,15 +11,18 @@ int main(void)
 	long int number = 612852475143;
 	long int largest;
 
-	while (number % 2 == 0 || number % 3 == 0 && number > 3)
+	while ((number % 2 == 0) || (number % 3 == 0))
 	{
-		if (number % 2 == 0)
+		if (number > 3)
 		{
-			number /= 2;
-		}
-		else
-		{
-			number /= 3;
+			if (number % 2 == 0)
+			{
+				number /= 2;
+			}
+			else
+			{
+				number /= 3;
+			}
 		}
 	}
 
