62c24878b87c96f6b8d3dcff0596cc46df75a911
Amiir25 <amirsadik636@gmail.com>
2024-06-27 17:46:18 +0300

    Update on 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,16 +16,12 @@ void more_numbers(void)
 
 		while (j < 15)
 		{
-			if (j < 10)
-			{
-				_putchar('0' + j);
-			}
-			else
+			if (j >= 10)
 			{
 				_putchar('0' + j / 10);
-				_putchar('0' + j % 10);
 			}
 
+			_putchar('0' + j % 10);
 			j++;
 		}
 		i++;
