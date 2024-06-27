c3d6bf919b4ce17f50e2227de13bb7bdec9cbec7
Amiir25 <amirsadik636@gmail.com>
2024-06-27 18:36:52 +0300

    Add 100-prime_factors.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factors.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int number = 612852475143;
+	long int largest;
+
+	while (number % 2 == 0 || number % 3 == 0 && number > 3)
+	{
+		if (number % 2 == 0)
+		{
+			number /= 2;
+		}
+		else
+		{
+			number /= 3;
+		}
+	}
+
+	largest = number;
+
+	printf("%li\n", largest);
+
+	return (0);
+}
