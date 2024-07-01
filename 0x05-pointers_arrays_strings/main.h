9d30c3b1f6251f953ef2f3827419bd7492391ae7
Amiir25 <amirsadik636@gmail.com>
2024-07-01 20:44:53 +0300

    Add 6-puts2.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void _putchar(char);
 void reset_to_98(int *n);
 void swap_int(int *, int *);
 int _strlen(char *);
 void _puts(char *);
 void print_rev(char *);
 void rev_string(char *);
+void puts2(char *);
 
 #endif
