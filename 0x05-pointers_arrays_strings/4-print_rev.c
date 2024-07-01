b6746666acad0bb5cffec652d7a7a29221445591
Amiir25 <amirsadik636@gmail.com>
2024-07-01 18:17:29 +0300

    Add 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - displays
+ * @s: pointer variable
+ */
+
+void print_rev(char *s)
+{
+	int i, j, k;
+	char rev[100];
+
+	i = strlen(s);
+	k = 0;
+	for (j = i - 1; j >= 0; j--)
+	{
+		rev[k] = s[j];
+		k++;
+	}
+
+	printf("%s\n", rev);
+}
