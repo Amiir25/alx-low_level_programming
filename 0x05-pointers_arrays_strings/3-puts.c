385250974857f80cfdd955333b64b449485c1c21
Amiir25 <amirsadik636@gmail.com>
2024-07-01 17:10:53 +0300

    Add 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - displays
+ * @str: pointer variable
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,5 +3,7 @@
 
 void reset_to_98(int *n);
 void swap_int(int *, int *);
+int _strlen(char *);
+void _puts(char *);
 
 #endif
