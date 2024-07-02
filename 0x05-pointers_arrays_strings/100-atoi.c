86c50b8b9a6f4af374601645a0407592d8481208
Amiir25 <amirsadik636@gmail.com>
2024-07-02 06:26:44 +0300

    Add 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _atoi - displays
+ * @s: pointer variable
+ *
+ * Return: int
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int started = 0;
+	
+	while (*s != '\0')
+	{
+		if (*s == '-')
+		{
+			if (started) 
+				break;
+			sign = -sign;
+		}
+
+		else if (*s == '+')
+		{
+			if (started)
+				break;
+		}
+
+		else if (*s >= '0' && *s <= '9')
+		{
+			started = 1;
+			result = result * 10 + (*s - '0');
+		}
+
+		else if (started)
+			break;
+            
+		s++;
+	}
+
+	return (sign * result);
+}
+
