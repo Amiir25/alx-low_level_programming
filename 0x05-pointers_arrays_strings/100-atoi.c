7887fc3b445f4d8d066278c3d95d19993aafd891
Amiir25 <amirsadik636@gmail.com>
2024-07-02 06:50:39 +0300

    Update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,34 +15,37 @@ int _atoi(char *s)
 	int sign = 1;
 	int result = 0;
 	int started = 0;
-	
+
 	while (*s != '\0')
 	{
 		if (*s == '-')
 		{
-			if (started) 
+			if (started)
 				break;
 			sign = -sign;
 		}
-
 		else if (*s == '+')
 		{
 			if (started)
 				break;
 		}
-
 		else if (*s >= '0' && *s <= '9')
 		{
 			started = 1;
+			if (result > (INT_MAX - (*s - '0')) / 10)
+			{
+				if (sign == 1)
+					return (INT_MAX);
+				else
+					return (INT_MIN);
+			}
 			result = result * 10 + (*s - '0');
 		}
 
 		else if (started)
 			break;
-            
 		s++;
 	}
-
 	return (sign * result);
 }
 
