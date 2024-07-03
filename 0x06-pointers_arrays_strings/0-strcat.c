dc2f237d970605daa2b40ebb8995b3bf1a0548bc
Amiir25 <amirsadik636@gmail.com>
2024-07-03 11:34:03 +0300

    Add 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - concatenates
+ * @dest: pointer variable
+ * @src: pointer variable
+ *
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	char *hold;
+
+	if ((hold = strchr(src, '\n')) != NULL)
+	{
+		*hold = '\0';
+	}
+
+	return (strcat(dest, src));
+}
