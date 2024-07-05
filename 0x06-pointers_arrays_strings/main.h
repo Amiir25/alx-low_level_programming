9b110d806d49f5ac3963981a207c4491fde2fd3c
Amiir25 <amirsadik636@gmail.com>
2024-07-05 04:21:06 +0300

    Add 101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void _putchar(char);
 char *_strcat(char *, char *);
 char *_strncat(char *, char *, int);
 char *_strncpy(char *, char *, int);
@@ -9,5 +10,7 @@ void reverse_array(int *, int);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *);
+void print_number(int);
 
 #endif
