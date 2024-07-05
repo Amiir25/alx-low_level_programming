b0a89017aba32d744f6871f3ae4dbd87038f0052
Amiir25 <amirsadik636@gmail.com>
2024-07-05 04:55:08 +0300

    Add 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *rot13 - rotates
+ * @s: pointer variable
+ *
+ * Return: char
+ */
+
+char *rot13(char *s)
+{
+	char *p = s;
+	while (*p)
+	{
+		if ((*p >= 'A' && *p <= 'Z') || (*p >= 'a' && *p <= 'z'))
+		{
+			if ((*p >= 'A' && *p <= 'M') || (*p >= 'a' && *p <= 'z'))
+			{
+				*p += 13;
+			}
+			else
+			{
+				*p -= 13;
+			}
+		}
+
+		p++;
+	}
+
+	return (s);
+}
