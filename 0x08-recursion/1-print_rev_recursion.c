1489527bd78e40187efb517e56e2fe9382c0d619
Amiir25 <amirsadik636@gmail.com>
2024-07-10 10:10:47 +0300

    Add 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - printd the reverse of a string
+ *@s: string to be reversed
+ *
+ * Return: Nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		puts("");
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	putchar(*s);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 void _puts_recursion(char *);
+void _print_rev_recursion(char *);
 
 #endif
