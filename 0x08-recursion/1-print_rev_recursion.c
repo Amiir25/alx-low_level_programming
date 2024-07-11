1edff7093b2270c3950324ac0397c07db55231de
Amiir25 <amirsadik636@gmail.com>
2024-07-11 04:07:03 +0300

    Update

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,8 +2,6 @@
 #include <string.h>
 #include "main.h"
 
-char *rev_string(char *, int, int);
-
 /**
  * _print_rev_recursion - printd the reverse of a string
  * @s: string to be reversed
@@ -13,40 +11,17 @@ char *rev_string(char *, int, int);
 
 void _print_rev_recursion(char *s)
 {
-	int len;
-	char *result;
-
-	len = strlen(s);
-	result = rev_string(s, 0, len - 1);
-}
-
-/**
- * rev_string - reverse a string
- * @s: string to be reversed
- * @start: index counter
- * @end: index counter
- *
- * Return: the reversed string
- */
-
-char *rev_string(char *s, int start, int end)
-{
-	char hold;
-
 	if (*s == '\0')
 	{
 		puts("");
-		return (s);
+		return;
 	}
-
-	if (start >= end)
+/*
+	if (*s == '\n')
 	{
-		return (s);
+		return;
 	}
-
-	hold = s[start];
-	s[start] = s[end];
-	s[end] = hold;
-
-	return (rev_string(s, start + 1, end - 1));
+*/
+	_print_rev_recursion(s + 1);
+	putchar(*s);
 }
