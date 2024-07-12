9e378d947d54a2da9f813b0c2aa06f58248341d1
Amiir25 <amirsadik636@gmail.com>
2024-07-12 20:33:43 +0300

    Add 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the product fo two arguments passed to the program
+ * @argc: the number of the elements of argv
+ * @argv: array of commads
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	if (argc == 3)
+	{
+		printf("%d\n", num1 * num2);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+
+	return (0);
+}
