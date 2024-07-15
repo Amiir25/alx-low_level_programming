88960bad746323e7197e183f7cc56ef7a9315505
Amiir25 <amirsadik636@gmail.com>
2024-07-15 15:56:18 +0300

    Add 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * *_strdup - 
+ * @str:
+ *
+ * Return: 
+ */
+
+char *_strdup(char *str)
+{
+	return (strdup(str));
+}
