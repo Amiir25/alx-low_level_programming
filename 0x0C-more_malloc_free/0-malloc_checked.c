442a08ed9732167fd6e726d5fc5470213ddb79a6
Amiir25 <amirsadik636@gmail.com>
2024-07-18 09:32:30 +0300

    Add 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - allocate memmory address dynamically
+ * @b: the number of bytes of the memory address
+ *
+ * Return: Nothing
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *address;
+
+	address =(malloc(b);
+	if (address == NULL)
+	{
+		exit (98);
+	}
+
+	return (address);
+}
