9aebf3663d03e38e6b161df2759b7ff3320e09c0
Amiir25 <amirsadik636@gmail.com>
2024-07-18 19:57:47 +0300

    Add 100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * *_realloc - reallocate a memory block
+ * @ptr: a pointer to the old address
+ * @old_size: the size of the old address
+ * @new_size: the size of the new address
+ *
+ * Return: a pointer to the reallocated memory block
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	ptr = realloc(ptr, old_size * new_size);
+	free(ptr);
+	return (ptr);
+}
