2c63b3c5c49a39ac5dee8f23776a72b4b28b2eb3
Amiir25 <amirsadik636@gmail.com>
2024-07-18 20:16:31 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,21 +14,23 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	if (old_size == new_size)
-	{
-		return (ptr);
-	}
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
 	if (ptr == NULL)
 	{
 		return (malloc(new_size));
 	}
 
-	ptr = realloc(ptr, old_size * new_size);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (old_size == new_size)
+	{
+		return (ptr);
+	}
+
+	ptr = realloc(ptr, old_size + new_size);
 	if (ptr == NULL)
 	{
 		return (NULL);
