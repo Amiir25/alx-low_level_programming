353d954a5f7d50b6f269ffc1b94a67ae7259d357
Amiir25 <amirsadik636@gmail.com>
2024-07-18 13:44:46 +0300

    Add 3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *array_range - allocate memeory for int array
+ * @min: the minimum value of the array
+ * @max: the maximum value of the array
+ *
+ * Return: pointer to the allocated address
+ */
+
+int *array_range(int min, int max)
+{
+	int i, j;
+	int *array;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	array = malloc((max + 1) * sizeof(int));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	j = min;
+	for (i = 0; i <= max; i++)
+	{
+		array[i] = j;
+		j++;
+	}
+
+	return (array);
+}
