a32bb483269f80587346ee582beab0911cdfa05b
Amiir25 <amirsadik636@gmail.com>
2024-07-25 15:51:17 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - struct data for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
 struct dog
 {
 	char *name;
@@ -11,5 +18,6 @@ struct dog
 void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *);
 typedef struct dog dog_t;
+dog_t *new_dog(char *, float, char *);
 
 #endif /* DOG_H */
