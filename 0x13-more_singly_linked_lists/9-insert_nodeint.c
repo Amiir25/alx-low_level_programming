9dd40a19d3f8a69b105f2d45c5514937d18029dc
Amiir25 <amirsadik636@gmail.com>
2024-08-06 18:54:19 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,7 +38,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			return (NULL);
 		}
-
 		count++;
 	}
 
@@ -52,6 +51,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		prev_node->next = new_node;
 		new_node->next = current_node;
 	}
-
 	return (new_node);
 }
