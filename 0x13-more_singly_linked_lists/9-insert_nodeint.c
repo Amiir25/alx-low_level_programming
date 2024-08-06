5ce2be8ebce3ccaca88781180030184b0b71ed3d
Amiir25 <amirsadik636@gmail.com>
2024-08-06 20:32:53 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,17 +28,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new_node-> = *head;
+		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
 
-	current_node = *head
+	current_node = *head;
 	for (i = 0; i < idx - 1 && current_node != NULL; i++)
+	{
+		current_node = current_node->next;
+	}
+
+	if (current_node == NULL || i != idx - 1)
 	{
 		free(new_node);
 		return (NULL);
 	}
+
 	new_node->next = current_node->next;
 	current_node->next = new_node;
 
