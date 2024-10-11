dfe0df1a7d411e5271c5365017811bc78a152c10
Amiir25 <amirsadik636@gmail.com>
2024-10-11 18:04:52 +0300

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -48,5 +48,6 @@ hash_table_t *hash_table_create(unsigned long int);
 unsigned long int hash_djb2(const unsigned char *);
 unsigned long int key_index(const unsigned char *, unsigned long int);
 int hash_table_set(hash_table_t *, const char *, const char *);
+hash_node_t *create_node(const char *, const char *);
 
 #endif  /* HASH_TABLES_H */
