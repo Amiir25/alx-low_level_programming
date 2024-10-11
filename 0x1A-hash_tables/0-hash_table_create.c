6190a6766f99f4b3f41adaf8ebf7df34b0f8f19e
Amiir25 <amirsadik636@gmail.com>
2024-10-11 10:24:53 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 
-	table = calloc(size, sizeof(hash_table_t));
+	table = calloc(size, sizeof(hash_node_t));
 	if (table == NULL)
 		return NULL;
 
