edaff57393f00603ac1a8296affddb81041f8fad
Amiir25 <amirsadik636@gmail.com>
2024-10-11 19:17:46 +0300

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -18,7 +18,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	i = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[i];
-	if (node != NULL)
+	while (node != NULL)
 	{
 		if (strcmp(node->key, key) == 0)
 			return (node->value);
