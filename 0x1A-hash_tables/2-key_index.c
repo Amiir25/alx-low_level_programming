b5610a12e90fbce9c4e85a892d4042cb2c61ba5b
Amiir25 <amirsadik636@gmail.com>
2024-10-11 14:49:16 +0300

    Update 2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,14 +10,8 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int i;
-	int j;
+	unsigned long int hash;
 
-	i = 0;
-	for (j = 0; key[j] != '\0'; j++)
-	{
-		i += key[j];
-	}
-
-	return (i % size);
+	hash = hash_djb2(key);
+	return (hash % size);
 }
