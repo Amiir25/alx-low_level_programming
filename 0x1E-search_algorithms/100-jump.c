dd1ddb29c9e9a76b2558b1b93dbb10de1987c8f7
Amir <amirsadik636@gmail.com>
2025-01-29 10:08:53 +0300

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,7 +14,7 @@ int jump_search(int *array, size_t size, int value)
 {
 	size_t step, first, last, i;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	step = sqrt(size);
