d3f207b663e85d2fe897410bd3891d8907f6dcaf
Amiir25 <144411155+Amiir25@users.noreply.github.com>
2025-03-03 21:50:10 +0300

    Update 1-main.c

--- 0x06-pointers_arrays_strings/1-main.c
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -15,10 +15,12 @@ int main(void)
 
 	printf("%s\n", s1);
 	printf("%s", s2);
+	
 	ptr = _strncat(s1, s2, 1);
 	printf("%s\n", s1);
 	printf("%s\n", s2);
 	printf("%s\n", ptr);
+	
 	ptr = _strncat(s1, s2, 1024);
 	printf("%s\n", s1);
 	printf("%s\n", s2);
